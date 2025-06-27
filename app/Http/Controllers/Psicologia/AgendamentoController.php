e2452cc5fb25c7ae80bf6f6c7f2148f3551014c0
NascimentoDavi <davirnascimentox@gmail.com>
2025-06-27 11:57:24 -0300

    feat: Adiciona Models

---
+++ app/Http/Controllers/Psicologia/AgendamentoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Psicologia;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AgendamentoController extends Controller
+{
+    public function getAgendamentos(): Agendaetn
+    {
+
+    }
+
+
+}
