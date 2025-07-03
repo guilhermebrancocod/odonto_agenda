1bf339d9148ff82b543fc95c5176c0cd585b93e0
NascimentoDavi <davirnascimentox@gmail.com>
2025-07-03 10:40:22 -0300

    feat: Adiciona ajuste de roteamento para Usuários únicos (acesso a somente uma clínica)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,16 +8,23 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        // ARMAZENA LOGIN DE USUARIO E ID DA CLINICA
         $usuario = session('usuario');
         $clinicas = $usuario->pluck('ID_CLINICA')->toArray();
 
+        // VERIFICA CLINICAS QUE USUÁRIO TEM ACESSO
         if(in_array(1, $clinicas) && in_array(2, $clinicas)) {
-            // dd("Acesso à ambas as clínicas");
             return redirect()->route('selecionar-clinica-get');
+
+        // CASO USUARIO POSSA ACESSAR SOMENTE PSICOLOGIA
         } else if (in_array(1, $clinicas)) {
-            dd("Acesso à clínica de Psicologia");
+           return redirect()->route('menu_agenda_psicologia');
+
+        // CASO USUARIO POSSA ACESSAR SOMENTE ODONTOLOGIA
         } else if (in_array(2, $clinicas)) {
-            dd("Acesso à clínica de Odontologia");
+            return redirect()->route('menu_agenda_odontologia');
+
+        // CASO DE ACESSO INVÁLIDO
         } else {
             dd("Usuário com acesso inválido");
         }
