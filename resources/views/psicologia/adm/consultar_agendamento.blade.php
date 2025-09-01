94941f9e5fa3ab053608407ddc1230955bea4ee3
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-09-01 16:42:53 -0300

    Feat: Adds "Novo" buttons
    Adds "Novo Agendamento" button to the schedule consultation page and "Novo Paciente" button to the patient consultation page, improving user experience by providing quick access to creation functionalities.

--- resources/views/psicologia/adm/consultar_agendamento.blade.php
+++ resources/views/psicologia/adm/consultar_agendamento.blade.php
@@ -82,6 +82,9 @@
     <div class="container ms-3 mw-100">
         <div class="row">
             <x-page-title>
+                    <p onclick="window.location.href = '/psicologia/criar-agendamento'" class="btn btn-success p-2 me-3" style="font-size: 15px;" >
+                        <span>Novo Agendamento</span>
+                    </p>
             </x-page-title>
 
             <div class="col-12 shadow-lg shadow-dark p-4 bg-body-tertiary rounded">
